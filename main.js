7f34dc43129d128df33d664f4802922156913743
Kateryna Gurienkova <gurienkova@gmail.com>
2021-06-29 08:31:42 +0300

    выношу гет тайм в отдельную ф-цию и добавляю условие, чтобы таймер не уходил в минус

--- main.js
+++ main.js
@@ -2,47 +2,71 @@ const refDays = document.querySelector('[data-value="days"]');
 const refHours = document.querySelector('[data-value="hours"]');
 const refMins = document.querySelector('[data-value="mins"]');
 const refSecs = document.querySelector('[data-value="secs"]');
+const refTimer = document.querySelector('.timer');
+//console.log(refTimer);
+ 
 
 class CountdownTimer {
   constructor(selector, holiday) {
     this.selector = selector;
     this.holiday = holiday;
   }
-  start() {
-    const holiday = new Date("Jul 24,2021").getTime();
 
-    setInterval(() => {
+  start() {
+    
+    this.interval = setInterval(() => {
+      //если время вышло
+      //const holiday = new Date("Jun 18,2021").getTime();
+
+      const holiday = new Date("Jul 18,2021").getTime();
       const currentDate = Date.now();
       const timerDate = holiday - currentDate;
-      console.log(timerDate);
-      const getDays = (time) => pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-      console.log(getDays(timerDate));
-      const getHours = (time) =>
-        pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-      console.log(getHours(timerDate));
-      const getMins = (time) =>
-        pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-      console.log(getMins(timerDate));
-      const getSecs = (time) => pad(Math.floor((time % (1000 * 60)) / 1000));
-      console.log(getSecs(timerDate));
-      refDays.textContent = getDays(timerDate);
-      refHours.textContent = getHours(timerDate);
-      refMins.textContent = getMins(timerDate);
-      refSecs.textContent = getSecs(timerDate);
+      //console.log(timerDate);
+      const {days,hours,mins,secs }=this.getTimeForTimer(timerDate);
+      refDays.textContent =  days ;
+      refHours.textContent = hours;
+      refMins.textContent =  mins ;
+      refSecs.textContent = secs;
+
+      this.finishOfTimer(timerDate);
+
     }, 100);
-  }
+
+  };
+
+  getTimeForTimer(time) {
+  const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+  console.log(days);
+  const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+  //console.log(getHours(timerDate));
+  const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+  //console.log(getMins(timerDate));
+  const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+  //console.log(getSecs(timerDate));
+    return { days, hours, mins, secs };
 }
 
 //ф-ция от Репеты возвращает 00 до 10
-function pad(value) {
+pad(value) {
   return String(value).padStart(2, "0");
 }
+//чтобы таймер не уходил в минус
+finishOfTimer(time) {
+  if (time <= 0) {
+    clearInterval(this.interval);
+    refTimer.textContent = 'HAPPY HOLIDAYS'
+  }
+};
+};
 
-// function setTimer({ days, hours, mins, secs }) {
-
-// }
 const timer = new CountdownTimer({
   selector: "#timer-1",
-  holiday: new Date("Jul 24, 2021"),
+  holiday: new Date("Jul 18, 2021"),
 });
 timer.start();
+// Если время вышло
+// const timer = new CountdownTimer({
+//   selector: "#timer-1",
+//   holiday: new Date("Jun 18, 2021"),
+// });
+// timer.start();
